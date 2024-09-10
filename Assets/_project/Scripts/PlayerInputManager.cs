98288c80dbfd54669a8e24c4b6cfb5117618a129
Jari Bou <bourdietome@gmail.com>
2024-09-10 19:47:24 +0200

    [ADD] basic player movement

---
+++ Assets/_project/Scripts/PlayerInputManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField, Range(0, 500)] private float _speed;
+    private PlayerInputs _playerInputs;
+    private Vector3 _moveVec;
+
+    private void Awake()
+    {
+        _playerInputs = new PlayerInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _moveVec;
+
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        Vector2 val = ctx.ReadValue<Vector2>();
+        _moveVec = new Vector3(val.x, _rb.velocity.y, val.y).normalized * _speed;
+        _rb.velocity = _moveVec;
+    }
+
+    private void OnEnable()
+    {
+        _playerInputs.PlayerMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInputs.PlayerMap.Disable();
+    }
+
+}
+    
